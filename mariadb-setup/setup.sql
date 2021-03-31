1e5c367af2b7fa952e864a5d129891c2f99b3f63
Hamilton Lima <hamilton.lima@gmail.com>
2021-03-30 20:43:08 -0400

    Create metabase database and user

--- mariadb-setup/setup.sql
+++ mariadb-setup/setup.sql
@@ -1,1 +1,5 @@
--- add initial setup actions here
+-- Add initial setup actions here
+CREATE DATABASE metabase;
+CREATE USER 'metabase'@'%' IDENTIFIED BY '9b88f4a41570';
+GRANT ALL ON metabase.* TO 'metabase'@'%';
+GRANT ALL ON geoffrey.* TO 'metabase'@'%';
